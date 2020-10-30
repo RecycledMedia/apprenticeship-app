7f76b6264cd621916654245e1f524ac346612306
T Lawler <teelawlz@users.noreply.github.com>
2020-10-29 18:14:07 -0700

    Add second test (deletes task)

--- cypress/integration/basic_tests_spec.js
+++ cypress/integration/basic_tests_spec.js
@@ -20,3 +20,30 @@ describe('User submits a new task', function() {
         cy.contains(pageObject.taskEntries, testTask).should.exist;
     })
 })
+
+describe('User deletes a task', function() {
+    it('no longer displays in the task list', function() {
+        // Construct test data
+        const testTask = `Make sure this task exists. ${Date.now()}`;
+        // Actual steps we perform in the browser
+        cy.visit('/');
+        cy.get(pageObject.taskEntries).then(({length: initialLength}) => {
+            cy.get(pageObject.textField).type(testTask);
+            cy.get(pageObject.submitTaskButton).click();
+            verifyEntriesCount(initialLength + 1);
+            cy.contains(pageObject.taskEntries, testTask).find(pageObject.deleteTaskButton).click();
+            verifyEntriesCount(initialLength);
+            cy.get(pageObject.taskEntries).then(entries => {
+                Object.values(entries).forEach(entry => {
+                    expect(entry.innerText).not.to.eq(testTask);
+                })
+            })
+        })
+    })
+})
+
+function verifyEntriesCount(expectedAmount) {
+    cy.get(pageObject.taskEntries).then(entries => {
+        expect(entries.length).to.eq(expectedAmount);
+    });
+}
